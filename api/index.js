ec60dd29d1a011ca7be2801658963b53cc7ef753
rando_mane <hogand@sou.edu>
2021-03-22 20:10:10 -0700

    inital commit

---
+++ api/index.js
@@ -0,0 +1,144 @@
+const express = require('express');
+const cors = require('cors');
+const NewsAPI = require('newsapi');
+const fetch = require('node-fetch');
+const language = require('@google-cloud/language');
+
+const app = express();
+app.use(cors());
+
+const newsapi = new NewsAPI(process.env.NEWS_API_KEY);
+const nlpapi = new language.LanguageServiceClient();
+
+app.get('/news/:ticker', async (req, res) => {
+
+  const ticker = req.params.ticker.toUpperCase();
+  const payload = {};
+
+  // The application has the user enter a ticker symbol but the news api
+  // gets much better results when you enter in the company name. Here I am making
+  // a request to the alphavantage api which will return information about the
+  // ticker symbol the user entered including the name of the company which I will
+  // use to query the news api
+
+  try {
+    const response = await fetch('https://www.alphavantage.co/query?' +
+                                 `function=OVERVIEW&symbol=${ticker}&` +
+                                 `apikey=${process.env.ALPHA_VANTAGE_API_KEY}`)
+
+    const parseCompany = await response.json();
+    const company = parseCompany.Name;
+    payload['companyName'] = company;
+
+  // in order to send a dynamic request to the news api, I need to calculate
+  // the current day, and the day 14-16 days previous to the current day
+  // then I need to format them into strings that the api will accept
+
+  const today = new Date();
+  const currentYear = today.getFullYear()
+  const currentMonth = today.getMonth()   // gets month index value
+  const currentDay = today.getDate()
+
+  if (currentDay < 14) {
+    startDay = 15 + currentDay;
+    if (currentMonth == 0) {
+      startYear = currentYear - 1;
+      startMonth = 11;
+    } else {
+      startYear = currentYear;
+      startMonth = currentMonth - 1;
+    }
+  } else {
+    startDay = currentDay - 13;
+    startMonth = currentMonth;
+    startYear = currentYear;
+  }
+
+  const fromString = `${startYear}-${startMonth + 1}-${startDay}`;
+  const toString = `${currentYear}-${currentMonth + 1}-${currentDay}`;
+
+  // once I have the company name and the strings for start and end date, I can make the request
+  // to the news api. The content I'm looking for in the response is an array called articles. Each
+  // element in the array is an object and the information I want is stored in title, description, and content
+
+  const newsResponse = await newsapi.v2.everything({
+    q: company,
+    from: fromString,
+    to: toString,
+    language: 'en',
+    sortBy: 'relevancy',
+    page: 2
+    })
+
+  const articles = newsResponse.articles;
+
+  // after I get the data from the news api, I want to collect all titles, descriptions, and content
+  // into one array that I can loop over to get the sentiment score for each. I also want to keep the
+  // articles grouped together when I send them to the client so they can be formatted properly on
+  // the screen. For each article I push the entire article to payload.articles and push the individual
+  // pieces to the allContent array.
+
+  const allContent = [];
+  payload['articles'] = [];
+
+  for (let i=0; i<articles.length; i++) {
+    payload['articles'].push(articles[i]);
+    allContent.push(articles[i].title)
+    allContent.push(articles[i].description)
+    allContent.push(articles[i].content)
+  }
+
+  // The google language api complains when I feed it null values. Here I am filtering out null values
+  // from the news articles content
+
+  const filteredContent = allContent.filter(element => element !== null);
+
+  // as I loop over each piece of content, I will be adding each value for the sentiment and magnitude to
+  // individual arrays. I will also be keeping a running total of the magnitude which will be used to
+  // calculate the aggregate sentiment
+
+  let totalMagnitude = 0.0;
+  let totalSentiment = 0.0;
+  const magnitudeScores = [];
+  const sentimentScores = [];
+
+  for (let i=0; i<filteredContent.length; i++) {
+
+    const document = {content: filteredContent[i],
+                      type: 'PLAIN_TEXT'
+    }
+
+    const [nlpresponse] = await nlpapi.analyzeSentiment({document: document});
+    const results = nlpresponse.documentSentiment;
+    totalMagnitude += results.magnitude;
+    magnitudeScores.push(results.magnitude);
+    sentimentScores.push(results.score);
+  }
+
+  // Once I have the total magnitude, I can divide each piece of content's magnitude by the total
+  // to get that pieces weight. Then I multiply the sentiment score it received by it's weight
+  // to get it's sentiment score in relation to all other sentiment scores and add it to the total.
+
+  for (let i=0; i<sentimentScores.length; i++) {
+    const magnitude = magnitudeScores[i] / totalMagnitude;
+    const sentiment = sentimentScores[i] * magnitude;
+    totalSentiment += sentiment;
+  }
+
+  payload['sentiment'] = totalSentiment;
+
+  // now that I have the company Name, all the articles for that company in the last two weeks and
+  // that company's aggregate sentiment score for the last two weeks, I can send a response to the client.
+
+  res.json(payload);
+
+  } catch (err) {
+    console.log(err.message);
+    res.status(400).json('failed');
+  }
+})
+
+
+app.listen(process.env.PORT, () => {
+  console.log('Server started on port', process.env.PORT);
+});
---
+++ client/src/App.js
@@ -0,0 +1,161 @@
+import './App.css';
+import React, {useState, useEffect} from 'react';
+const TradingView = window.TradingView;
+
+function App() {
+
+  // BASE_URL stores the location of the express api, ticker is a value set by the user,
+  // posNeg and longShort are string variables used to output messages on the screen
+  // about the particlular stock the user entered, and payload is going to store all the
+  // data received from the api, if the response has a status 400, error is set to true
+  // which is used to conditionally render an error message on the screen.
+
+  const [BASE_URL, _] = useState('http://localhost:3002');
+  const [ticker, setTicker] = useState('GME');
+  const [posNeg, setPosNeg] = useState(' ');
+  const [longShort, setLongShort] = useState(' ');
+  const [payload, setPayload] = useState({
+    companyName: ' ',
+    articles: [],
+    sentiment: 0
+  });
+
+  const [error, setError] = useState(false);
+
+  // This code creates a TradingViews object which is a class imported with a script tag
+  // from the internet. The script tag is in the root index.html file and is imported into
+  // this react component at the top.
+
+  const tradingViewSetup = () => {
+    new TradingView.widget(
+      {
+      "width": 1000,
+      "height": 600,
+      "symbol": `${ticker}`,
+      "interval": "D",
+      "timezone": "Etc/UTC",
+      "theme": "dark",
+      "style": "1",
+      "locale": "en",
+      "toolbar_bg": "#f1f3f6",
+      "enable_publishing": false,
+      "allow_symbol_change": true,
+      "container_id": "tradingview_ae533"
+      }
+    )
+  }
+  useEffect(() => {
+    tradingViewSetup();
+  },[]);
+
+  // when a user types letteres into the input, they are automatically converted
+  // to uppercase b/c all tickers are always uppercase.
+
+  const onChange = (e) => {
+    setTicker(e.target.value.toUpperCase());
+  }
+
+  // When the user submits a new ticker, the payload object is initially reset.
+  // The tradingViewSetup function is called again to get a new chart for the new
+  // ticker, error is set to false in case it was previously set to true, a fetch
+  // request is made to the api and it will wait for the payload response. If theres
+  // no error, payload is set to the response object, and posNeg and longShort are
+  // set to the appropriate strings based on the value for sentiment.
+
+  const onSubmit = async (e) => {
+    e.preventDefault()
+
+    setPosNeg(' ');
+    setLongShort(' ');
+    setPayload({companyName: ' ',
+                articles: [],
+                sentiment: 0.0
+              })
+    
+    tradingViewSetup()
+
+    try {
+      setError(false);
+
+      const response = await fetch(BASE_URL + `/news/${ticker}`)
+
+      if (response.status === 400) {
+        setError(true);
+
+      } else {
+        const parseRes = await response.json()
+        setPayload(parseRes);
+
+        if (parseFloat(parseRes.sentiment) > 0) {
+          console.log('positive sentiment:', parseRes.sentiment);
+          setPosNeg('Positive');
+          setLongShort('Long');
+        } else {
+          console.log('negative sentiment:', parseRes.sentiment);
+          setPosNeg('Negative');
+          setLongShort('Short');
+        }
+      }
+    } catch (err) {
+      console.log(err.message);
+      setError(true);
+    }
+  }
+
+  // All the articles received for the ticker the user entered will be stored in an array.
+  // This function is mapped over that array and creates a display on the screen for each
+  // article displaying title, description, content, and the url link to that article.
+
+  const makeArticle = (article) => {
+    return <>
+      <h2>{article.title}</h2>
+      <h3>{article.description}</h3>
+      <p>{article.content}</p>
+      <a href={article.url} target='_blank'>continue reading</a>
+      <hr />
+    </>
+  }
+
+  return <>
+    <h1>MILLION DOLLAR STOCK PREDICTOR</h1>
+    <h2>Enter the ticker symbol of your favorite stock and see whether it will go up or down</h2>
+
+    {/* form for the user to enter a ticker */}
+
+    <form onSubmit={onSubmit}>
+    <input value={ticker} onChange={onChange} ></input>
+      <button type='submit'>Submit</button>
+    </form>
+
+    {/* if error is true an error message will display on the screen. */}
+
+    {error && <p class='error'>There was a problem retrieving sentiment data for this ticker</p>}
+
+    {/* divs for the trading view widget */}
+
+    <div class="tradingview-widget-container">
+      <div id="tradingview_ae533"></div>
+    </div>
+
+    <div id='news-sentiment'>
+
+    {/* div for displaying the news. all articles in articles array are mapped to the makeArticle function. */}
+
+      <div id='news'>
+        <h1>News</h1>
+        {payload.articles && payload.articles.map(makeArticle)}
+      </div>
+
+      {/* displays sentiment score received, whether its positive or negative and a long/short recommendation. */}
+
+      <div id='sentiment'>
+        <h1>Sentiment</h1>
+        <h1>{payload.sentiment}</h1>
+        <h2>{payload.companyName} has a {posNeg} sentiment score.</h2>
+        <h2>You should take a {longShort} position.</h2> 
+      </div>
+    </div>
+  </>
+}
+
+export default App;
